90c5a609fc91cf560de54fc2a18809c7dd11baf6
Big-Boronto <114494949+Big-Boronto@users.noreply.github.com>
2023-12-14 10:57:15 +0200

    Add project files.

---
+++ VT_Sequences.cpp
@@ -0,0 +1,141 @@
+#pragma region Boilerplate
+#include "pch.h"
+#include "framework.h"
+#include <Windows.h>
+#include <stdio.h>
+#include <sstream>
+
+#define ESC "\x1b"
+
+//Variables
+bool AlternateBufferActive = false;
+int  HorizontalResolution = 32;
+int  VerticalResolution = 32;
+#pragma endregion
+
+static bool EnableVirtual()
+{
+	// Set output mode to handle virtual terminal sequences
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hOut == INVALID_HANDLE_VALUE)
+	{
+		return false;
+	}
+	HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE);
+	if (hIn == INVALID_HANDLE_VALUE)
+	{
+		return false;
+	}
+
+	DWORD dwOriginalOutMode = 0;
+	DWORD dwOriginalInMode = 0;
+	if (!GetConsoleMode(hOut, &dwOriginalOutMode))
+	{
+		return false;
+	}
+	if (!GetConsoleMode(hIn, &dwOriginalInMode))
+	{
+		return false;
+	}
+
+	DWORD dwRequestedOutModes = ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN;
+	DWORD dwRequestedInModes = ENABLE_VIRTUAL_TERMINAL_INPUT;
+
+	DWORD dwOutMode = dwOriginalOutMode | dwRequestedOutModes;
+	if (!SetConsoleMode(hOut, dwOutMode))
+	{
+		// we failed to set both modes, try to step down mode gracefully.
+		dwRequestedOutModes = ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+		dwOutMode = dwOriginalOutMode | dwRequestedOutModes;
+		if (!SetConsoleMode(hOut, dwOutMode))
+		{
+			// Failed to set any VT mode, can't do anything here.
+			return false;
+		}
+	}
+
+	DWORD dwInMode = dwOriginalInMode | dwRequestedInModes;
+	if (!SetConsoleMode(hIn, dwInMode))
+	{
+		// Failed to set VT input mode, can't do anything here.
+		return false;
+	}
+
+	return true;
+}
+
+static void SetConsoleSize(int X, int Y)
+{
+	std::ostringstream command;
+	command << ESC << "[8;" << X << ";" << Y << "t";
+	printf(command.str().c_str());
+}
+
+static bool SwitchScreenBuffer()
+{
+	if (!AlternateBufferActive)
+	{
+		printf(ESC "[?1049h");
+		AlternateBufferActive = true;
+		return true;
+	}
+	else
+	{
+		printf(ESC "[?1049l");
+		AlternateBufferActive = false;
+		return false;
+	}
+}
+
+static int  SetConsoleColor(int ForegroundColor, int BackgroundColor)
+{
+	switch (ForegroundColor)
+	{
+	case -1: break; // Do Nothing
+	case  0: printf(ESC "[39m"); break; // Reset
+	case  1: printf(ESC "[30m"); break; // Black
+	case  2: printf(ESC "[31m"); break; // Red
+	case  3: printf(ESC "[32m"); break; // Green
+	case  4: printf(ESC "[33m"); break; // Yellow
+	case  5: printf(ESC "[34m"); break; // Blue
+	case  6: printf(ESC "[35m"); break; // Magenta
+	case  7: printf(ESC "[36m"); break; // Cyan
+	case  8: printf(ESC "[37m"); break; // White
+	case  9: printf(ESC "[90m"); break; // Bright Black
+	case 10: printf(ESC "[91m"); break; // Bright Red
+	case 11: printf(ESC "[92m"); break; // Bright Green
+	case 12: printf(ESC "[93m"); break; // Bright Yellow
+	case 13: printf(ESC "[94m"); break; // Bright Blue
+	case 14: printf(ESC "[95m"); break; // Bright Magenta
+	case 15: printf(ESC "[96m"); break; // Bright Cyan
+	case 16: printf(ESC "[97m"); break; // Bright White
+	}
+	switch (BackgroundColor)
+	{
+	case -1: break; // Do Nothing
+	case  0: printf(ESC "[49m"); break; // Reset
+	case  1: printf(ESC "[40m"); break; // Black
+	case  2: printf(ESC "[41m"); break; // Red
+	case  3: printf(ESC "[42m"); break; // Green
+	case  4: printf(ESC "[43m"); break; // Yellow
+	case  5: printf(ESC "[44m"); break; // Blue
+	case  6: printf(ESC "[45m"); break; // Magenta
+	case  7: printf(ESC "[46m"); break; // Cyan
+	case  8: printf(ESC "[47m"); break; // White
+	case  9: printf(ESC "[100m"); break; // Bright Black
+	case 10: printf(ESC "[101m"); break; // Bright Red
+	case 11: printf(ESC "[102m"); break; // Bright Green
+	case 12: printf(ESC "[103m"); break; // Bright Yellow
+	case 13: printf(ESC "[104m"); break; // Bright Blue
+	case 14: printf(ESC "[105m"); break; // Bright Magenta
+	case 15: printf(ESC "[106m"); break; // Bright Cyan
+	case 16: printf(ESC "[107m"); break; // Bright White
+	}
+
+	return 0;
+}
+
+static void NegativeConsole()
+{
+	printf(ESC "[7m");
+}
